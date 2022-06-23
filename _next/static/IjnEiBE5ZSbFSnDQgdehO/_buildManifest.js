7d3b699fecb945a9a5f6b5b94d58c34918a49633
Tarun Gupta <61280743+thistarungupta@users.noreply.github.com>
2022-06-23 17:07:26 +0530

    Uploaded code

---
+++ _next/static/IjnEiBE5ZSbFSnDQgdehO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-513fc3031ce4af68.js"],"/_error":["static/chunks/pages/_error-815e492bede44f3e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
